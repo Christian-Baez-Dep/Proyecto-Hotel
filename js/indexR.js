04fb805cbaa693fe31a0cb84df217fe766190f31
MellMc <marinezmmel@gmail.com>
2024-03-21 23:33:56 -0400

    Actualizando archivos indexR.html e indexR.js

--- js/indexR.js
+++ js/indexR.js
@@ -6,14 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
 
         const name = form.querySelector('input[name = "name"]').value;
+        const lastName = form.querySelector('input[name = "lastName"]').value;
         const email = form.querySelector('input[name = "email"]').value;
         const password = form.querySelector('input[name = "password"]').value;
+        const cedula = form.querySelector('input[name = "cedula"]').value;
         const gender = form.querySelector('input[name = "gender"]:checked').value;
 
         const data = {
             name,
+            lastName,
             email,
             password,
+            cedula,
             gender
         };
 
